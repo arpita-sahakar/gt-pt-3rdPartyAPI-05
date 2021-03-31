5d5e556c96cc29970b8bad2af197dd641bd3a0eb
Arpita Saha <arpita.sahaa@gmail.com>
2021-03-31 11:26:40 -0400

    unsolved exer02

--- 03-Algorithms/02-max-num/Unsolved/max-num.js
+++ 03-Algorithms/02-max-num/Unsolved/max-num.js
@@ -1,3 +1,15 @@
 // Write code to return the largest number in the given array
 
-var maxNum = function(arr) {};
+// var maxNum = function(arr) {};
+
+const maxNum = (arr)=>{
+    let largestNum = arr[0];
+    for (i=0; i<arr.length; i++){
+        if(arr[i]>largestNum){
+            largestNum= arr[i]
+        }
+    }
+    console.log(largestNum);
+};
+
+maxNum([5,10,2,15,17])
