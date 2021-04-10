917242564bf7245fa6284436b8437f2ffc7491a1
Arpita Saha <arpita.sahaa@gmail.com>
2021-04-10 12:33:07 -0400

    algo

--- 03-Algorithms/03-vowel-count/Unsolved/vowel-count.js
+++ 03-Algorithms/03-vowel-count/Unsolved/vowel-count.js
@@ -1,3 +1,20 @@
 // Write code to return the the number of vowels in `str`
 
-var vowelCount = function(str) {};
+// var vowelCount = function(str) {};
+
+let vowelCount = (str) => {
+  let vowel = "aeiou";
+  let vowelCount = 0;
+  let vowelsPresent = [];
+
+  for (i = 0; i < str.length; i++) {
+    if (vowel.includes(str[i].toLowerCase())) {
+      vowelCount = vowelCount + 1;
+      vowelsPresent.push(str[i])
+    }
+  }
+  console.log(vowelCount);
+  console.log(vowelsPresent);
+};
+
+vowelCount("ArpitaKar");
